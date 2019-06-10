ead648983a10e1fe77adc0410838c1777807ada6
Robel-T <robel.teklehaimanot@heig-vd.ch>
2019-06-10 19:38:05 +0200

    Add the load balancing: multiple server nodes

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,18 +1,32 @@
 <?php
     $dynamic_app = getenv('DYNAMIC_APP');
-    $static_app = getenv('STATIC_APP');
+    $static_app  = getenv('STATIC_APP');
+    
+    $dynamic_app2 = getenv('DYNAMIC_APP2');
+    $static_app2  = getenv('STATIC_APP2');
 ?>
 
 <VirtualHost *:80>
     
     ServerName lab.res.ch
 
+    <Proxy "balancer://dynamicset">
+        BalancerMember 'http://<?php print "$dynamic_app"?>'
+        BalancerMember 'http://<?php print "$dynamic_app2"?>'
+    </Proxy>
+
+    <Proxy "balancer://staticset">
+        BalancerMember 'http://<?php print "$static_app"?>'
+        BalancerMember 'http://<?php print "$static_app2"?>'
+    </Proxy>
+
+
+    ProxyPass '/api/country/' 'balancer://dynamicset/'
+    ProxyPassReverse '/api/country/' 'balancer://dynamicset/'
+
     
-    ProxyPass '/api/country/' 'http://<?php print "$dynamic_app"?>/'
-    ProxyPassReverse '/api/country/' 'http://<?php print "$dynamic_app"?>/'
-    
-    ProxyPass '/' 'http://<?php print "$static_app"?>/'
-    ProxyPassReverse '/' 'http://<?php print "$static_app"?>/'
+    ProxyPass '/' 'balancer://staticset/'
+    ProxyPassReverse '/' 'balancer://staticset/'
     
 </VirtualHost>
 
